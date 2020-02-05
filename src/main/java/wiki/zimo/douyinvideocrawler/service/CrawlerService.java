b0aa66e6010c5df28db04757782d986adc7ba145
子墨 <461009747@qq.com>
2020-02-05 13:42:20 +0800

    only use jsoup to request

--- src/main/java/wiki/zimo/douyinvideocrawler/service/CrawlerService.java
+++ src/main/java/wiki/zimo/douyinvideocrawler/service/CrawlerService.java
@@ -1,6 +1,7 @@
 package wiki.zimo.douyinvideocrawler.service;
 
 import com.alibaba.fastjson.JSONObject;
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -8,11 +9,7 @@ import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 @Service
 public class CrawlerService {
@@ -20,83 +17,45 @@ public class CrawlerService {
     private String BASE_API_URL;
 
     public String demo2(String url) throws IOException {
-        HttpURLConnection conn = getConn(url);
-        conn.connect();
-        int code = conn.getResponseCode();
-        if (code == HttpURLConnection.HTTP_OK) {
-            InputStream inputStream = conn.getInputStream();
-            String resultHtml = inputscreamToString(inputStream);
-            Document doc = Jsoup.parse(resultHtml);
-            Element video = doc.getElementById("theVideo");
-            url = video.attr("src").replace("playwm", "play");
-            conn = getConn(url);
-            conn.connect();
-            code = conn.getResponseCode();
-            if (code == HttpURLConnection.HTTP_MOVED_TEMP) {
-                String location = conn.getHeaderField("Location");
-                return location;
-            }
-        }
-        return null;
+        Connection conn = getConn(url);
+        Document doc = conn.get();
+        Element video = doc.getElementById("theVideo");
+        url = video.attr("src").replace("playwm", "play");
+        conn = getConn(url);
+        return getRedirectURL(conn);
     }
 
     public String demo1(String url) throws IOException {
-        HttpURLConnection conn = getConn(url);
-        conn.connect();
-        int code = conn.getResponseCode();
-        if (code == HttpURLConnection.HTTP_OK) {
-            InputStream inputStream = conn.getInputStream();
-            String resultHtml = inputscreamToString(inputStream);
-            Document doc = Jsoup.parse(resultHtml);
-            Elements scriptElements = doc.getElementsByTag("script");
-            for (Element e : scriptElements) {
-                String text = e.toString();
-                if (text.contains("douyin_falcon:page/reflow_video/index")) {
-                    text = text.substring(text.indexOf('{'), text.lastIndexOf('}')).replaceAll("\\s+", "");
-                    text = text.substring(text.indexOf('{', text.indexOf(')')), text.lastIndexOf('}') + 1);
-                    JSONObject json = JSONObject.parseObject(text);
-                    String itemId = json.getString("itemId");
-                    String dytk = json.getString("dytk");
-                    url = BASE_API_URL.replace("{}", itemId);
-                    url = url.replace("{}", dytk);
-                    conn = getConn(url);
-                    conn.connect();
-                    code = conn.getResponseCode();
-                    if (code == HttpURLConnection.HTTP_OK) {
-                        text = inputscreamToString(conn.getInputStream());
-                        json = JSONObject.parseObject(text).getJSONArray("item_list").getJSONObject(0);
-                        json = json.getJSONObject("video").getJSONObject("play_addr_lowbr");
-                        url = json.getJSONArray("url_list").getString(0);//.replace("play", "playwm");
-                        conn = getConn(url);
-                        conn.connect();
-                        code = conn.getResponseCode();
-                        if (code == HttpURLConnection.HTTP_MOVED_TEMP) {
-                            String location = conn.getHeaderField("Location");
-                            return location;
-                        }
-                    }
-                }
+        Connection conn = getConn(url);
+        Document doc = conn.get();
+        Elements scriptElements = doc.getElementsByTag("script");
+        for (Element e : scriptElements) {
+            String text = e.toString();
+            if (text.contains("douyin_falcon:page/reflow_video/index")) {
+                text = text.substring(text.indexOf('{'), text.lastIndexOf('}')).replaceAll("\\s+", "");
+                text = text.substring(text.indexOf('{', text.indexOf(')')), text.lastIndexOf('}') + 1);
+                JSONObject json = JSONObject.parseObject(text);
+                String itemId = json.getString("itemId");
+                String dytk = json.getString("dytk");
+                url = BASE_API_URL.replace("{}", itemId);
+                url = url.replace("{}", dytk);
+                conn = getConn(url);
+                text = conn.get().body().text();
+                json = JSONObject.parseObject(text).getJSONArray("item_list").getJSONObject(0);
+                json = json.getJSONObject("video").getJSONObject("play_addr");
+                url = json.getJSONArray("url_list").getString(0);
+                return getRedirectURL(getConn(url));
             }
         }
         return null;
     }
 
-    private HttpURLConnection getConn(String url) throws IOException {
-        URL baseURL = new URL(url);
-        HttpURLConnection conn = (HttpURLConnection) baseURL.openConnection();
-        conn.setRequestProperty("User-Agent",
-                "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1");
-        return conn;
+    private Connection getConn(String url) {
+        return Jsoup.connect(url).userAgent("Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1").ignoreContentType(true);
     }
 
-    private String inputscreamToString(InputStream in) throws IOException {
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        int len = 0;
-        byte[] bt = new byte[1024];
-        while ((len = in.read(bt)) != -1) {
-            out.write(bt, 0, len);
-        }
-        String content = new String(out.toByteArray());
-        return content;
+    private String getRedirectURL(Connection conn) throws IOException {
+        return conn.followRedirects(false).execute().header("location");
     }
+
 }

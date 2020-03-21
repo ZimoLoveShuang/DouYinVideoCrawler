9745a8fe5b115486ed29574e789c62b0fd18f5b0
子墨 <461009747@qq.com>
2020-03-21 14:46:11 +0800

    加入页面

---
+++ src/main/java/wiki/zimo/douyinvideocrawler/controller/IndexController.java
@@ -0,0 +1,13 @@
+package wiki.zimo.douyinvideocrawler.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping(path = {"/", "/index", "/index.html"})
+    public String index() {
+        return "index";
+    }
+}
+
